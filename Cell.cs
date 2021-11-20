44279dd5a1ffff4de9be3af6eec34668f507f166
SilasAkerman <94759651+SilasAkerman@users.noreply.github.com>
2021-11-20 20:25:02 +0100

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
+
+namespace Maze_Generation
+{
+    class Cell
+    {
+        int _x;
+        int _y;
+        int _size;
+        bool[] walls = { true, true, true, true };
+
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool Visited { get; set; }
+        public bool Highlighted { get; set; } // The current cell
+        public bool Backtracked { get; set; }
+
+        public Cell(int row, int col, int size)
+        {
+            Row = row;
+            Column = col;
+            _size = size;
+            _x = col * size;
+            _y = row * size;
+        }
+
+        public void Display()
+        {
+            if (Visited) Raylib.DrawRectangle(_x, _y, _size, _size, Color.GREEN);
+            if (Backtracked) Raylib.DrawRectangle(_x, _y, _size, _size, Color.DARKBLUE);
+            if (Highlighted) Raylib.DrawRectangle(_x, _y, _size, _size, Color.RED);
+            if (walls[0]) Raylib.DrawLine(_x, _y, _x + _size, _y, Color.WHITE);
+            if (walls[1]) Raylib.DrawLine(_x + _size, _y, _x + _size, _y + _size, Color.WHITE);
+            if (walls[2]) Raylib.DrawLine(_x + _size, _y + _size, _x, _y + _size, Color.WHITE);
+            if (walls[3]) Raylib.DrawLine(_x, _y + _size, _x, _y, Color.WHITE);
+        }
+
+        public List<Cell> GetUnvisitedNeighbours(Cell[,] grid)
+        {
+            int[,] options = new int[4, 2]
+            {
+                { 1, 0 },
+                { -1, 0 },
+                { 0, 1 },
+                { 0, -1 }
+            };
+            List<Cell> neighbours = new List<Cell>();
+            for (int i = 0; i < options.GetLength(0); i++)
+            {
+                int nextRow = options[i, 0] + Row;
+                int nextCol = options[i, 1] + Column;
+                if (!(nextRow < 0 || nextCol < 0 || nextRow > grid.GetLength(0)-1 || nextCol > grid.GetLength(1)-1))
+                {
+                    if (!grid[nextRow, nextCol].Visited)
+                    {
+                        neighbours.Add(grid[nextRow, nextCol]);
+                    }
+                }
+            }
+            return neighbours;
+        }
+
+        public void RemoveWallBetween(Cell chosenCell)
+        {
+            if (Row < chosenCell.Row)
+            {
+                walls[2] = false;
+                chosenCell.walls[0] = false;
+            }
+            else if (Row > chosenCell.Row)
+            {
+                walls[0] = false;
+                chosenCell.walls[2] = false;
+            }
+            else if (Column > chosenCell.Column)
+            {
+                walls[3] = false;
+                chosenCell.walls[1] = false;
+            }
+            else if (Column < chosenCell.Column)
+            {
+                walls[1] = false;
+                chosenCell.walls[3] = false;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Raylib_cs;
+
+namespace Maze_Generation
+{
+    class Program
+    {
+        const int WIDTH = 1800;
+        const int HEIGHT = 900;
+        const int SPACING = 30;
+
+        static int rows;
+        static int cols;
+
+        static Cell[,] grid;
+        static List<Cell> stack = new List<Cell>();
+        static Cell currentCell;
+
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            rows = HEIGHT / SPACING;
+            cols = WIDTH / SPACING;
+            grid = new Cell[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    grid[row, col] = new Cell(row, col, SPACING);
+                }
+            }
+            grid[0, 0].Visited = true;
+            stack.Add(grid[0, 0]);
+
+            Raylib.InitWindow(WIDTH, HEIGHT, "Maze Generation - First Depth Search");
+            //Raylib.SetTargetFPS(3);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                //while (stack.Count > 0)
+                //{
+                    Update();
+                //}
+                Display();
+            }
+            Raylib.CloseWindow();
+        }
+
+        static void Update()
+        {
+            if (stack.Count > 0)
+            {
+                currentCell = stack[stack.Count - 1];
+                stack.RemoveAt(stack.Count - 1);
+                List<Cell> neighbours = currentCell.GetUnvisitedNeighbours(grid);
+                if (neighbours.Count > 0)
+                {
+                    stack.Add(currentCell);
+                    Cell chosenCell = neighbours[random.Next(neighbours.Count)];
+                    currentCell.RemoveWallBetween(chosenCell);
+                    currentCell.Highlighted = false;
+                    currentCell = chosenCell;
+                    currentCell.Visited = true;
+                    currentCell.Highlighted = true;
+                    stack.Add(currentCell);
+                }
+                else
+                {
+                    currentCell.Highlighted = false;
+                    currentCell.Backtracked = true;
+                }
+            }
+        }
+
+        static void Display()
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.BLACK);
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    grid[row, col].Display();
+                }
+            }
+            //Raylib.DrawFPS(10, 10);
+            Raylib.EndDrawing();
+        }
+    }
+}
